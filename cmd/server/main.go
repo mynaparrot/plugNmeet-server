5de0a616a746ede2e51e9769b3bf2c9f22f3792c
jibon Costa <jiboncosta57@gmail.com>
2022-02-27 21:55:17 +0600

    cli option added

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"github.com/mynaparrot/plugNmeet/internal/config"
 	"github.com/mynaparrot/plugNmeet/internal/controllers"
 	"github.com/mynaparrot/plugNmeet/internal/factory"
 	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
 	"gopkg.in/yaml.v3"
 	"io/ioutil"
 	"os"
@@ -15,9 +15,36 @@ import (
 )
 
 func main() {
-	configFile := flag.String("config", "config.yaml", "Configuration file")
-	flag.Parse()
-	readYaml(configFile)
+	cli.VersionPrinter = func(c *cli.Context) {
+		fmt.Printf("%s\n", c.App.Version)
+	}
+
+	app := &cli.App{
+		Name:        "plugnmeet-server",
+		Usage:       "Scalable, Open source web conference system",
+		Description: "without option will start server",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "config",
+				Usage:       "Configuration file",
+				DefaultText: "config.yaml",
+				Value:       "config.yaml",
+			},
+		},
+		Action:  startServer,
+		Version: Version,
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func startServer(c *cli.Context) error {
+	err := readYaml(c.String("config"))
+	if err != nil {
+		return err
+	}
 
 	// set mysql factory connection
 	factory.NewDbConnection()
@@ -42,28 +69,25 @@ func main() {
 		sig := <-sigChan
 		log.Infoln("exit requested, shutting down", "signal", sig)
 		_ = router.Shutdown()
+		log.Exit(1)
 	}()
 
-	err := router.Listen(fmt.Sprintf(":%d", config.AppCnf.Client.Port))
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	log.Infoln("Running cleanup tasks...")
-	log.Exit(1)
+	return router.Listen(fmt.Sprintf(":%d", config.AppCnf.Client.Port))
 }
 
-func readYaml(filename *string) {
+func readYaml(filename string) error {
 	var appConfig config.AppConfig
-	yamlFile, err := ioutil.ReadFile(*filename)
+	yamlFile, err := ioutil.ReadFile(filename)
 
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
 
 	err = yaml.Unmarshal(yamlFile, &appConfig)
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
 	config.SetAppConfig(&appConfig)
+
+	return nil
 }
