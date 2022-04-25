a0cdf536b424153fa4a515f778461221a1444f28
jibon Costa <jiboncosta57@gmail.com>
2022-04-25 18:51:21 +0600

    Feat: pdf file upload support for whiteboard

--- internal/models/manage_files.go
+++ internal/models/manage_files.go
@@ -1,16 +1,19 @@
 package models
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/gabriel-vasile/mimetype"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 	"github.com/mynaparrot/plugNmeet/internal/config"
 	log "github.com/sirupsen/logrus"
 	"io"
 	"io/ioutil"
 	"mime/multipart"
 	"os"
+	"os/exec"
 	"sort"
 	"strings"
 )
@@ -19,12 +22,17 @@ type ManageFile struct {
 	Sid                string `json:"sid" validate:"required"`
 	RoomId             string `json:"roomId" validate:"required"`
 	UserId             string `json:"userId" validate:"required"`
+	FilePath           string `json:"file_path"`
 	Resumable          bool   `json:"resumable"`
 	uploadFileSettings *config.UploadFileSettings
+	fileExtension      string
+	fileMimeType       string
+	rs                 *RoomService
 }
 
 func NewManageFileModel(m *ManageFile) *ManageFile {
 	m.uploadFileSettings = &config.AppCnf.UploadFileSettings
+	m.rs = NewRoomService()
 	return m
 }
 
@@ -54,14 +62,22 @@ type resumableUploadReq struct {
 	ResumableCurrentChunkSize int64
 }
 
+type UploadedFileResponse struct {
+	FilePath      string
+	FileName      string
+	FileExtension string
+	FileMimeType  string
+}
+
 // ResumableFileUpload method can only be use if you are using resumable.js as your frontend.
 // Library link: https://github.com/23/resumable.js
-func (m *ManageFile) ResumableFileUpload(c *fiber.Ctx) (string, string, error) {
+func (m *ManageFile) ResumableFileUpload(c *fiber.Ctx) (*UploadedFileResponse, error) {
 	req := new(resumableUploadReq)
+	res := new(UploadedFileResponse)
 	err := c.QueryParser(req)
 	if err != nil {
 		fmt.Println(err)
-		return "", "", err
+		return nil, err
 	}
 
 	tempFolder := os.TempDir()
@@ -94,14 +110,14 @@ func (m *ManageFile) ResumableFileUpload(c *fiber.Ctx) (string, string, error) {
 			room, _ := rm.GetRoomInfo(m.RoomId, m.Sid, 1)
 			if room.Id == 0 {
 				_ = c.SendStatus(fiber.StatusBadRequest)
-				return "", "", errors.New("room isn't running")
+				return nil, errors.New("room isn't running")
 			}
 
 			// check if file size is OK
 			if req.ResumableTotalSize > int64(m.uploadFileSettings.MaxSize*1024*1024) {
 				_ = c.SendStatus(fiber.StatusBadRequest)
 				msg := fmt.Sprintf("file is too big. Max allow %dMB", m.uploadFileSettings.MaxSize)
-				return "", "", errors.New(msg)
+				return nil, errors.New(msg)
 			}
 		}
 
@@ -109,13 +125,13 @@ func (m *ManageFile) ResumableFileUpload(c *fiber.Ctx) (string, string, error) {
 		if err != nil {
 			log.Errorln(err)
 			_ = c.SendStatus(fiber.StatusServiceUnavailable)
-			return "", "", err
+			return nil, err
 		}
 		file, err := reqf.Open()
 		if err != nil {
 			log.Errorln(err)
 			_ = c.SendStatus(fiber.StatusServiceUnavailable)
-			return "", "", err
+			return nil, err
 		}
 		defer file.Close()
 
@@ -125,7 +141,7 @@ func (m *ManageFile) ResumableFileUpload(c *fiber.Ctx) (string, string, error) {
 			err = m.validateMimeType(fo)
 			if err != nil {
 				_ = c.SendStatus(fiber.StatusUnsupportedMediaType)
-				return "", "", err
+				return nil, err
 			}
 		}
 
@@ -135,7 +151,7 @@ func (m *ManageFile) ResumableFileUpload(c *fiber.Ctx) (string, string, error) {
 			if err != nil {
 				_ = c.SendStatus(fiber.StatusInternalServerError)
 				log.Errorln(err)
-				return "", "", err
+				return nil, err
 			}
 		}
 
@@ -145,29 +161,35 @@ func (m *ManageFile) ResumableFileUpload(c *fiber.Ctx) (string, string, error) {
 		if err != nil {
 			log.Errorln(err)
 			_ = c.SendStatus(fiber.StatusServiceUnavailable)
-			return "", "", err
+			return nil, err
 		}
 		defer f.Close()
 		_, err = io.Copy(f, file)
 		if err != nil {
 			log.Errorln(err)
 			_ = c.SendStatus(fiber.StatusServiceUnavailable)
-			return "", "", err
+			return nil, err
 		}
 
 		if req.ResumableChunkNumber == req.ResumableTotalChunks {
 			// combining chunks into one file
 			err = m.combineResumableFiles(path, req.ResumableFilename, req.ResumableTotalChunks)
 			if err != nil {
-				return "", "", err
+				return nil, err
 			}
 		} else {
-			return "part_uploaded", "", err
+			res.FilePath = "part_uploaded"
+			return res, err
 		}
 	}
 
 	finalPath := fmt.Sprintf("%s/%s", m.Sid, req.ResumableFilename)
-	return finalPath, req.ResumableFilename, nil
+	res.FilePath = finalPath
+	res.FileName = req.ResumableFilename
+	res.FileExtension = m.fileExtension
+	res.FileMimeType = m.fileMimeType
+
+	return res, nil
 }
 
 func (m *ManageFile) validateMimeType(file multipart.File) error {
@@ -180,17 +202,19 @@ func (m *ManageFile) validateMimeType(file multipart.File) error {
 
 	allowedTypes := m.uploadFileSettings.AllowedTypes
 	sort.Strings(allowedTypes)
-	ext := strings.Replace(mtype.Extension(), ".", "", 1)
+
+	m.fileMimeType = mtype.String()
+	m.fileExtension = strings.Replace(mtype.Extension(), ".", "", 1)
 	allows := false
 
 	for _, t := range allowedTypes {
-		if ext == t {
+		if m.fileExtension == t {
 			allows = true
 			continue
 		}
 	}
 	if !allows {
-		if ext == "" {
+		if m.fileExtension == "" {
 			return errors.New("invalid file")
 		}
 		return errors.New(mtype.Extension() + " file type not allow")
@@ -247,4 +271,97 @@ func (m *ManageFile) DeleteFile(filePath string) error {
 func (m *ManageFile) DeleteRoomUploadedDir() error {
 	path := fmt.Sprintf("%s/%s", m.uploadFileSettings.Path, m.Sid)
 	return os.RemoveAll(path)
+}
+
+type convertStatus struct {
+	status bool
+	msg    string
+}
+type ConvertWhiteboardFileRes struct {
+	FileName   string `json:"file_name"`
+	FileId     string `json:"file_id"`
+	FilePath   string `json:"file_path"`
+	TotalPages int    `json:"total_pages"`
+}
+
+func (m *ManageFile) ConvertWhiteboardFile() (*ConvertWhiteboardFileRes, error) {
+	file := fmt.Sprintf("%s/%s", m.uploadFileSettings.Path, m.FilePath)
+	info, err := os.Stat(file)
+	if err != nil {
+		return nil, err
+	}
+
+	mtype, err := mimetype.DetectFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	fileId := uuid.NewString()
+	dir := fmt.Sprintf("%s/%s/%s", m.uploadFileSettings.Path, m.Sid, fileId)
+	err = os.MkdirAll(dir, os.ModePerm)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(mtype.Extension())
+
+	status := make(chan convertStatus)
+	go func(file, outputDir string) {
+		cmd := exec.Command("/usr/bin/mutool", "convert", "-o", outputDir+"/page_%d.svg", file)
+		_, err = cmd.Output()
+
+		if err != nil {
+			status <- convertStatus{status: false, msg: err.Error()}
+			return
+		}
+		status <- convertStatus{status: true, msg: "success"}
+	}(file, dir)
+
+	resStatus := <-status
+	if !resStatus.status {
+		return nil, errors.New(resStatus.msg)
+	}
+
+	totalPages, _ := os.ReadDir(dir)
+
+	res := &ConvertWhiteboardFileRes{
+		FileName:   info.Name(),
+		FilePath:   fmt.Sprintf("%s/%s", m.Sid, fileId),
+		FileId:     fileId,
+		TotalPages: len(totalPages),
+	}
+
+	// update metadata with info
+	_ = m.updateRoomMetadataWithOfficeFile(res)
+
+	return res, nil
+}
+
+func (m *ManageFile) updateRoomMetadataWithOfficeFile(f *ConvertWhiteboardFileRes) error {
+	room, err := m.rs.LoadRoomInfoFromRedis(m.RoomId)
+	if err != nil {
+		return err
+	}
+
+	data := make([]byte, len(room.Metadata))
+	copy(data, room.Metadata)
+
+	roomMeta := new(RoomMetadata)
+	err = json.Unmarshal(data, roomMeta)
+	if err != nil {
+		return err
+	}
+	roomMeta.Features.WhiteboardFeatures.WhiteboardFileId = f.FileId
+	roomMeta.Features.WhiteboardFeatures.FileName = f.FileName
+	roomMeta.Features.WhiteboardFeatures.FilePath = f.FilePath
+	roomMeta.Features.WhiteboardFeatures.TotalPages = f.TotalPages
+
+	metadata, _ := json.Marshal(roomMeta)
+	_, err = m.rs.UpdateRoomMetadata(m.RoomId, string(metadata))
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
