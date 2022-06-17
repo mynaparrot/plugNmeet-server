acfa91ac0dc8b0c8536aba73cf183648ddbd7337
jibon Costa <jiboncosta57@gmail.com>
2022-06-17 13:29:39 +0600

    update

--- internal/controllers/manage_files.go
+++ internal/controllers/manage_files.go
@@ -7,7 +7,6 @@ import (
 	"github.com/mynaparrot/plugNmeet/internal/models"
 	"net/url"
 	"os"
-	"strconv"
 	"strings"
 )
 
@@ -79,14 +78,13 @@ func HandleDownloadUploadedFile(c *fiber.Ctx) error {
 	otherParts, _ = url.QueryUnescape(otherParts)
 
 	file := fmt.Sprintf("%s/%s/%s", config.AppCnf.UploadFileSettings.Path, sid, otherParts)
-	fileInfo, err := os.Lstat(file)
+	_, err := os.Lstat(file)
 	if err != nil {
-		_ = c.SendStatus(fiber.StatusNotFound)
 		ms := strings.SplitN(err.Error(), "/", -1)
-		return c.SendString(ms[3])
+		return c.Status(fiber.StatusNotFound).SendString(ms[3])
 	}
 
-	c.Set("Content-Disposition", "attachment; filename="+strconv.Quote(fileInfo.Name()))
+	c.Attachment(file)
 	return c.SendFile(file, true)
 }
 
