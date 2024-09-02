d9d0ea0cbf69ab7b07fe6eba6d90863ad40a4813
Jibon L. Costa <jiboncosta57@gmail.com>
2024-09-02 14:59:35 +0200

    wait before process

--- pkg/models/webhook_room_end.go
+++ pkg/models/webhook_room_end.go
@@ -15,6 +15,7 @@ func (m *WebhookModel) roomFinished(event *livekit.WebhookEvent) {
 		log.Warnln(fmt.Sprintf("invalid webhook info received: %+v", event))
 		return
 	}
+	time.Sleep(time.Second * 1)
 
 	rInfo, err := m.natsService.GetRoomInfo(event.Room.Name)
 	if err != nil || rInfo == nil {
