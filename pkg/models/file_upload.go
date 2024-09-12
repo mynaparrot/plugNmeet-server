13c3de2e77d9efd3e3e491604b116d9156902602
Jibon L. Costa <jiboncosta57@gmail.com>
2024-09-12 22:10:44 +0200

    use `temp` inside upload setting's dir

--- pkg/models/file_upload.go
+++ pkg/models/file_upload.go
@@ -46,7 +46,7 @@ func (m *FileModel) ResumableFileUpload(c *fiber.Ctx) (*UploadedFileResponse, er
 		return nil, errors.New("RoomId or RoomSid is empty")
 	}
 
-	tempFolder := os.TempDir()
+	tempFolder := fmt.Sprintf("%s/tmp", m.app.UploadFileSettings.Path)
 	chunkDir := fmt.Sprintf("%s/%s", tempFolder, req.ResumableIdentifier)
 
 	switch c.Method() {
