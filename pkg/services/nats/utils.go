312c54b88ef681bf1683e8de371a3527cb1a71ec
Jibon L. Costa <jiboncosta57@gmail.com>
2025-06-04 23:50:04 +0600

    fix logic

--- pkg/services/nats/utils.go
+++ pkg/services/nats/utils.go
@@ -19,11 +19,11 @@ func (s *NatsService) getKV(bucket string) (jetstream.KeyValue, error) {
 // getStringValue retrieves a string value from the KeyValue store for the given key.
 // Returns an empty string if the key is not found.
 func (s *NatsService) getStringValue(kv jetstream.KeyValue, key string) (string, error) {
-	val, err := kv.Get(s.ctx, key)
-	if err != nil || val == nil {
-		return "", err
+	if val, err := kv.Get(s.ctx, key); err == nil && val != nil {
+		return string(val.Value()), nil
 	}
-	return string(val.Value()), nil
+
+	return "", nil
 }
 
 // getBoolValue retrieves a boolean value from the KeyValue store for the given key.
@@ -32,6 +32,9 @@ func (s *NatsService) getBoolValue(kv jetstream.KeyValue, key string) (bool, err
 	val, err := s.getStringValue(kv, key)
 	if err != nil {
 		return false, err
+	}
+	if len(val) == 0 {
+		return false, nil
 	}
 	return strconv.ParseBool(val)
 }
@@ -43,13 +46,21 @@ func (s *NatsService) getUint64Value(kv jetstream.KeyValue, key string) (uint64,
 	if err != nil {
 		return 0, err
 	}
+	if len(val) == 0 {
+		return 0, nil
+	}
 	return strconv.ParseUint(val, 10, 64)
 }
 
+// getInt64Value retrieves a int64 value from the KeyValue store for the given key.
+// Returns 0 if the key is not found or the value cannot be parsed as a int64.
 func (s *NatsService) getInt64Value(kv jetstream.KeyValue, key string) (int64, error) {
 	val, err := s.getStringValue(kv, key)
 	if err != nil {
 		return 0, err
+	}
+	if len(val) == 0 {
+		return 0, nil
 	}
 	return strconv.ParseInt(val, 10, 64)
 }
