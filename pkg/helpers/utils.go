11b8b8e417c96e9c6d15258294ff3e30c6e26ad3
Jibon L. Costa <jiboncosta57@gmail.com>
2026-01-13 09:19:33 +0600

    update

--- pkg/helpers/utils.go
+++ pkg/helpers/utils.go
@@ -2,7 +2,15 @@ package helpers
 
 import "math"
 
-func ToFixed(num float64, precision int) float64 {
-	output := math.Pow(10, float64(precision))
-	return math.Round(num*output) / output
+func ToFixed(num float32, precision int) float32 {
+	pow := math.Pow(10, float64(precision))
+	var rounded float64
+	if num < 0 {
+		rounded = float64(num)*pow - 0.5
+	} else {
+		rounded = float64(num)*pow + 0.5
+	}
+	truncated := math.Trunc(rounded)
+	result := truncated / pow
+	return float32(result)
 }
